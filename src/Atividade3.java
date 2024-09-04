c3315ed5148bce20c3a871f6fb55749ca3a63b2a
osvaldinho11 <joaovitorperico@gmail.com>
2024-09-04 20:50:14 -0300

    Vetores

---
+++ src/Atividade3.java
@@ -0,0 +1,14 @@
+public class Atividade3 {
+    public static void main(String[] args) {
+
+        int[] valores = new int[2];
+
+        valores[0] = 5;
+        valores[1] = 10;
+
+        int valor = valores[0];
+
+
+
+    }
+}
